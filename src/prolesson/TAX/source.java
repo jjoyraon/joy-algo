ee40664670b70e164c67663ff1ae2a4b5e262b7a
dongin.jung <dongin.jung@samsung.com>
2016-11-09 21:06:38 +0900

    tax, picnic

--- src/prolesson/TAX/source.java
+++ src/prolesson/TAX/source.java
@@ -3,10 +3,6 @@ package prolesson.TAX;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
 
 /*
  * 세금  
@@ -14,8 +10,8 @@ import java.util.Scanner;
  */
 public class source {
 
-	private static int LEAF_START;
-	private static long[] tree;
+
+	private static final long MIN = -1000000000l * 50l;
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("src/prolesson/TAX/input.txt"));
@@ -29,58 +25,41 @@ public class source {
 		for(int i=1; i<=N; i++){
 			arr[i] = Long.parseLong(split[i-1]);
 		}
-		int BIN = 1;
-		for(; BIN<N ; BIN*=2);
-		LEAF_START = BIN;
-		tree = new long[BIN*3];
+		long[] A = new long[K+2];
+		long[] B = new long[K+2];
+		long[] C = new long[K+2];
+		for(int i = 1; i<=K; i++){
+			A[i] = MIN; 
+			B[i] = MIN;
+		}
 		
 		for(int i=1; i<=N; i++){
-			update(i, arr[i]);
-		}
-		
-		long maxBorder = Long.MIN_VALUE;
-		PriorityQueue<Long> minHeap = new PriorityQueue<>(64);
-		for(int i=1; i<=N; i++){
-			for(int j=1; j<=i; j++){
-				long sum = select(j, i);
-				if(sum<maxBorder){
-					continue;
-				}
-				minHeap.offer(sum);
-				if(minHeap.size()>K){
-					Long poll = minHeap.poll();
-					maxBorder = Math.max(poll,  maxBorder);
+			long n = arr[i];
+			for(int j=1; j<=K; j++){
+				A[j] += n; 
+			}
+			for(int j=1; j<=K; j++){
+				if(A[j]<n){
+					for(int p = K; p>=j; p--){
+						A[p+1] = A[p];
+					}
+					A[j] = n;
+					break;
 				}
 			}
+			for(int j=1, l=1, r=1; j<=K; j++){
+				if(A[l]>B[r]){
+					C[j] = A[l++];
+				}else{
+					C[j] = B[r++];
+				}
+			}
+			for(int j=1; j<=K; j++){
+				B[j] = C[j];
+			}
+			
 		}
 		
-		System.out.println(minHeap.poll());
-	}
-	private static void update(int i, long newValue){
-		int next = LEAF_START - 1 + i;
-		tree[next] = newValue;
-		next = next/2;
-		while(next>0){
-			tree[next] = tree[next*2] + tree[next*2+1];
-			next = (int)next/2;
-		}
-	}
-	private static long select(int left, int right){
-		int l = LEAF_START -1 + left;
-		int r = LEAF_START -1 + right;
-		long sum = 0;
-		while(l<=r){
-			if(l%2 == 1){
-				sum += tree[l];
-				l++;
-			}
-			if(r%2 == 0){
-				sum += tree[r];
-				r--;
-			}
-			l = l/2;
-			r = r/2;
-		}
-		return sum;
+		System.out.println(B[K]);
 	}
 }
---
+++ src/prolesson/picnic/source.java
@@ -0,0 +1,86 @@
+package prolesson.picnic;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * 피크닉  
+ * http://koitp.org/problem/SDS_PRO_4_2/read/
+ */
+public class source {
+	
+	private static ArrayList<ArrayList<Integer>> nlist;
+	private static int N;
+	private static boolean[][] D;
+	private static int[] starts;
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("src/prolesson/picnic/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] knm = br.readLine().split(" ");
+		int K = Integer.parseInt(knm[0]);
+		N = Integer.parseInt(knm[1]);
+		int M = Integer.parseInt(knm[2]);
+		
+		D = new boolean[K+1][N+1];
+		
+		starts = new int[K+1];
+		for(int i=1; i<=K; i++){
+			starts[i] = Integer.parseInt(br.readLine());
+		}
+		
+		nlist = new ArrayList<>();
+		for(int i=0; i<=N; i++){
+			nlist.add(new ArrayList<Integer>());
+		}
+		
+		
+		for(int i=1; i<=M; i++){
+			String[] se = br.readLine().split(" ");
+			int s = Integer.parseInt(se[0]);
+			int e = Integer.parseInt(se[1]);
+			nlist.get(s).add(e);
+		}
+		
+		for(int i=1; i<=K; i++){
+			// 소의 시작 위치별로 bfs를 돌려서 닿는 모든 경로를 표시
+			bfs(i);
+		}
+		
+		int count = 0;
+		for(int i=1; i<=N; i++){
+			int countPerNode = 0;
+			for(int j=1; j<=K; j++){
+				if(D[j][i]){
+					countPerNode++;
+				}
+			}
+			if(countPerNode==K){
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+	private static void bfs(int k) {
+		
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(starts[k]);
+		D[k][starts[k]] = true;
+		while(!q.isEmpty()){
+			Integer poll = q.poll();
+			for(int n : nlist.get(poll)){
+				if(D[k][n]){
+					continue;
+				}
+				D[k][n] = true;
+				q.offer(n);
+			}
+		}
+	}
+}
